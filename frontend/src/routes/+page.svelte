dce6f4c43a7b433ce38aacd21421e53bcb48c53f
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2024-06-28 21:25:54 +0000

    create the ui from scratch

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	let count = 0;
+
+function handleClick() {
+    // calling this function will trigger an
+    // update if the markup references `count`
+    count = count + 1;
+}
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>Counter {count}</p>
+<button on:click={() => count++}>increase</button>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
