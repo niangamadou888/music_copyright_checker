b44eb3814ab9f804abf5ec96db31bad2a91c04ba
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2024-06-29 10:16:21 +0000

    home page done

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Navigation() {
   return (
-    <nav>
-        navigation here
+    <nav className='h-[70px] w-full bg-gradient-to-r from-[#24C9EF] via-[#3484F0] to-[#3A6AF1] flex flex-row justify-end items-center'>
+        <button className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+        <p>login</p>
+        </button>
     </nav>
   )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,7 +5,28 @@ function Home() {
   return (
     <div>
         <Navigation/>
-        <div>home</div>
+        <div className='w-full h-screen flex flex-col justify-center items-center gap-4'>
+          <h1 className='text-4xl font-sans font-extrabold text-black uppercase text-center'>Check music<span className='bg-gradient-to-r from-[#24C9EF] via-[#3484F0] to-[#3A6AF1] bg-clip-text text-transparent block text-3xl'>Copyright</span></h1>
+          <div className='w-1/2 flex flex-col gap-4'>
+            <div className='w-full flex'>
+              <input type="text" className='bg-[#24C9EF]/5 flex-grow border-gray-400 focus:border-[#3A6AF1] border-2 rounded-md h-12 focus:outline-none p-2 text-[#3A6AF1] font-semibold' />
+              <button className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+                <p>search</p>
+              </button>
+            </div>
+            <div className='flex flex-row justify-start gap-4'>
+              <label htmlFor="title" className="flex flex-row gap-2 items-center cursor-pointer">
+                <input type="radio" name="search_by" id="title" className="radio radio-info radio-xs" defaultChecked />
+                <p>Search by song title</p>
+              </label>
+              <label htmlFor="link" className="flex flex-row gap-2 items-center cursor-pointer">
+                <input type="radio" name="search_by" id="link" className="radio radio-info  radio-xs" />
+                <p>Search by youtube link</p>
+              </label>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
