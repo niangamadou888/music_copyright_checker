42ae331245cfd87a1d49ada38cc808c5003b7a2b
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2024-06-29 11:32:25 +0000

    put dist into backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,10 +25,10 @@ app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
 app.use('/youtube', youtubeRoutes);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 (async () => {
