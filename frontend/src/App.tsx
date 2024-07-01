3597217fb2b1506f05d5e538e85a5c2517a08c96
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2024-07-01 23:07:51 +0000

    context added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,36 @@ import {
 import Home from "./pages/Home";
 import Account from "./pages/Account";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from "react";
+import AuthContext from "./context/AuthContext";
+import { TUser } from "./types";
 
 export default function App() {
+  const [user, setUser] = useState<TUser | undefined>(undefined)
+
+  const login = (data: TUser) => {
+    localStorage.setItem('user', JSON.stringify(data))
+    setUser(data)
+  }
+
+  const logout = () => {
+    localStorage.removeItem('user')
+    setUser(undefined)
+  }
+
+  useEffect(() => {
+    const data = localStorage.getItem('user')
+    if (data) {
+      try {
+        const user = JSON.parse(data || '{}')
+        setUser(user)
+      } catch(error) {
+        console.error({error})
+      }
+    }
+  }, [])
 
   const queryClient = new QueryClient()
 
@@ -25,8 +51,10 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <AuthContext.Provider value={{user, login, logout}}>
        <ToastContainer />
       <RouterProvider router={router} />
+      </AuthContext.Provider>
     </QueryClientProvider>
   )
 }
--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,10 +1,17 @@
+import { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import AuthContext from '../context/AuthContext'
 
 function Navigation() {
+  const {user, logout} = useContext(AuthContext)
 
   const handleLogin = () => {
     // @ts-ignore
     document.getElementById('login')?.showModal()
+  }
+
+  const handleLogout = () => {
+    logout()
   }
 
   const handleRegister = () => {
@@ -14,18 +21,22 @@ function Navigation() {
 
   return (
     <nav className='h-[70px] w-full px-3 bg-gradient-to-r from-[#24C9EF] via-[#3484F0] to-[#3A6AF1] flex flex-row justify-end items-center gap-4'>
-        <Link to="/account/studio" onClick={handleRegister} className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
+        {user && <><Link to="/account/studio" onClick={handleRegister} className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
         <p>My Account</p>
         </Link>
-        <button onClick={handleRegister} className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
+         <button onClick={handleLogout} className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
+         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-arrow-right-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+         <p>logout</p>
+         </button></>}
+        {!user && <><button onClick={handleRegister} className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
         <p>register</p>
         </button>
         <button onClick={handleLogin} className='btn bg-[#24C9EF] hover:bg-[#3484F0] text-black hover:text-white'>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-arrow-right-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>
         <p>login</p>
-        </button>
+        </button></>}
     </nav>
   )
 }
