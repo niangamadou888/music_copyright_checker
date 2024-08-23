d3904d6b1497789605bca579d6d140a81a057840
AdonaysG <adonyas2000@gmail.com>
2024-08-23 16:11:04 -0700

    Add logout functionality and update header component

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { isFakeTouchstartFromScreenReader } from '@angular/cdk/a11y';
+import { Component, OnInit } from '@angular/core';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor(private toastService: ToastService) {}
+
+  ngOnInit(): void {
+    this.isAuth();
+    
+  }
+  isLogged: boolean = false;
+
+  isAuth(): boolean {
+    let token: string | null = null;
+
+    if (typeof window !== 'undefined') {
+      token = localStorage.getItem('token');
+    }
+
+    if (token) {
+      this.isLogged = true;
+      return true;
+    }
+
+    this.isLogged = false;
+    return false;
+  }
+
+  logOut(): void {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('token');
+      this.toastService.showToast('success', 'Logged out successfully');
+      setTimeout(() => {
+        window.location.href = '/';
+      } , 2000);
+    }
+  }
 }
