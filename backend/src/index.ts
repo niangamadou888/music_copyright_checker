6c5e34719501f37795075da18a616650c22fab71
abrhamgg <abrhamgg@gmail.com>
2024-09-14 16:50:42 -0400

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,10 +29,10 @@ app.use('/youtube', youtubeRoutes);
 app.use('/music', musicRoutes);
 app.use('/tags', tagRoutes);
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send('Server is running');
 });
 
 (async () => {
