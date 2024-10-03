ed65fbd6769ae53ab3f281bf81b34152443e47ac
Abrham Gebregiorgis <abrhamgg@gmail.com>
2024-10-03 11:48:03 -0400

    Update environment.prod.ts

--- frontend/src/enviroments/environment.prod.ts
+++ frontend/src/enviroments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://music-licence-checker.onrender.com/', // Your production API URL
+    apiUrl: 'https://music-licence-checker.onrender.com', // Your production API URL
   };
   
